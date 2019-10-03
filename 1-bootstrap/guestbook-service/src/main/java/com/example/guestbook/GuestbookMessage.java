5f541e6554736c945d18662c5ec1fe7f62ff31a1
Eduardo Wallace da Silva <eduardo.wallace@hdi.com.br>
2019-10-02 23:36:39 -0300

    ajuste

--- 1-bootstrap/guestbook-service/src/main/java/com/example/guestbook/GuestbookMessage.java
+++ 1-bootstrap/guestbook-service/src/main/java/com/example/guestbook/GuestbookMessage.java
@@ -2,12 +2,10 @@ package com.example.guestbook;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "guestbook_message")
 @Data
 public class GuestbookMessage {
 	@Id
